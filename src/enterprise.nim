618fd16f835db122e54c22987a6b37f245b8d62f
Frank Fischer <fischer.frank@gmail.com>
2025-02-09 13:10:42 +0100

    -

--- src/enterprise.nim
+++ src/enterprise.nim
@@ -4,11 +4,10 @@ import coords
 type
   Energy* = range[0..3000]
   Photon_Torpedoes* = range[0..10]
-  Quadrant = tuple[x: range[1..8], y:range[1..8]]
 
 randomize()
-var quadrant* = random_quadrant()
-var sector* = random_sector()
+var quadrant* = random_xy_coords()
+var sector* = random_xy_coords()
 var docked* = false
 var energy* = high(Energy)
 var photon_torpedoes* = high(Photon_Torpedoes)
---
+++ src/galaxy.nim
@@ -0,0 +1,16 @@
+import coords
+
+type
+  Quadrant = object
+    klingon_count: Natural = 0
+    starbase_count: Natural = 0
+    star_count: Natural = 0
+
+proc random_quadrant(): Quadrant =
+  discard
+
+type Galaxy
+proc random_galaxy():
+
+let quadrants = array[high(Coord), high[Coord],]
+
